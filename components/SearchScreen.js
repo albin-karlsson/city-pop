491b8013a4310e49164d4a7b62f4767528ccf7e5
Albin Karlsson <55614148+albin-karlsson@users.noreply.github.com>
2022-04-08 20:54:06 +0200

    Add comments for next steps

--- components/SearchScreen.js
+++ components/SearchScreen.js
@@ -2,12 +2,17 @@ import React, { useState } from "react";
 import { View, Text, Button, TextInput } from "react-native";
 import { Alert } from "react-native";
 
+// Pass country or city as route from home screen
 function SearchScreen({ route, navigation }) {
   const { mode } = route.params;
   const [searchTerm, setSearchTerm] = useState("");
 
   const showResults = () => {
     if (searchTerm.length > 1) {
+      // Make API call to check if any results
+      // Send those results to the results page
+      // If no results, promt the user to add a new country or city
+
       navigation.navigate("Results");
     } else {
       Alert.alert("Ooops...", `Please enter a ${mode}`);
