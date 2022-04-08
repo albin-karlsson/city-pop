dc9ffe1643e6097a17cb46482ef047190dd6280c
Albin Karlsson <55614148+albin-karlsson@users.noreply.github.com>
2022-04-08 20:51:32 +0200

    Add validation for text input

--- components/SearchScreen.js
+++ components/SearchScreen.js
@@ -1,14 +1,24 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Button, TextInput } from "react-native";
+import { Alert } from "react-native";
 
-function SearchScreen({ navigation }) {
+function SearchScreen({ route, navigation }) {
+  const { mode } = route.params;
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const showResults = () => {
+    if (searchTerm.length > 1) {
+      navigation.navigate("Results");
+    } else {
+      Alert.alert("Ooops...", `Please enter a ${mode}`);
+    }
+  };
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Search Screen</Text>
-      <Button
-        title="Go to Results"
-        onPress={() => navigation.navigate("Results")}
-      />
+      <Text>Search by {mode}</Text>
+      <TextInput placeholder={`Enter a ${mode}`} onChangeText={setSearchTerm} />
+      <Button title="Go to Results" onPress={showResults} />
     </View>
   );
 }
