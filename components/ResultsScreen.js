73f3e649d4dc2a24c1efc2840793852da9bc5674
Albin Karlsson <55614148+albin-karlsson@users.noreply.github.com>
2022-04-08 20:12:44 +0200

    Change behaviour of back button

--- components/ResultsScreen.js
+++ components/ResultsScreen.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function ResultsScreen() {
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("beforeRemove", (e) => {
+      e.preventDefault(); // Prevent default action
+      unsubscribe(); // Unsubscribe the event on first call to prevent infinite loop
+      navigation.navigate("Home"); // Navigate to the desired screen
+    });
+  }, []);
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Results Screen</Text>
