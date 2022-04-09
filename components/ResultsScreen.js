834d4b751aa93793121c22aa82ff7d303f981931
Albin Karlsson <55614148+albin-karlsson@users.noreply.github.com>
2022-04-09 13:04:43 +0200

    Add country search

--- components/ResultsScreen.js
+++ components/ResultsScreen.js
@@ -1,8 +1,10 @@
-import React, { useEffect } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Text, FlatList } from "react-native";
 
 function ResultsScreen({ route, navigation }) {
-  const { mode, searchTerm } = route.params;
+  const { mode, searchTerm, data } = route.params;
+  let [country, setCountry] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
 
   useEffect(() => {
     const unsubscribe = navigation.addListener("beforeRemove", (e) => {
@@ -10,17 +12,57 @@ function ResultsScreen({ route, navigation }) {
       unsubscribe(); // Unsubscribe the event on first call to prevent infinite loop
       navigation.navigate("Home"); // Navigate to the home screen (don't just go back one step)
     });
+
+    formatData();
   }, []);
+
+  const formatData = () => {
+    // If mode is not city
+    if (mode !== "city") {
+      determineCountry();
+      const filteredData = data.filter((el) => {
+        return el.fclName.includes("city") && el.countryName.includes(country);
+      });
+
+      setFilteredData(filteredData);
+    }
+    // Show city population results
+    else {
+    }
+  };
+
+  const determineCountry = () => {
+    country = data
+      .map((el) => {
+        return el.countryName;
+      })
+      .sort(
+        (a, b) =>
+          data.filter((v) => v === a).length -
+          data.filter((v) => v === b).length
+      )
+      .pop();
+
+    setCountry(country);
+  };
 
   const ResultsDisplay = () => {
     if (mode !== "city") {
-      return <Text>Displaying {mode} options...</Text>;
-    } else return <Text>Displaying cities..</Text>;
+      return (
+        <FlatList
+          keyExtractor={(item) => item.geonameId}
+          data={filteredData}
+          renderItem={(item) => {
+            <Text>{item.name}</Text>;
+          }}
+        />
+      );
+    } else return <Text>Displaying city population results..</Text>;
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>{searchTerm.toUpperCase()}</Text>
+      <Text style={styles.header}>{country.toUpperCase()}</Text>
       <View>{ResultsDisplay()}</View>
     </View>
   );
